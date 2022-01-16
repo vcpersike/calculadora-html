704c5f6df0fe841a16fbf643e47f87807401afa1
vcpersike <vcpersike@gmail.com>
2022-01-16 19:10:53 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function insert(num)
+{
+    var numero = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = numero + num;
+}
+function clean()
+{
+    document.getElementById('resultado').value = "";
+}
+function back()
+{
+    var resultado = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = resultado.substring(0, resultado.length -1);
+}
+function calcular()
+{
+    var resultado = document.getElementById('resultado').value;
+    if(resultado)
+    {
+        document.getElementById('resultado').value = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('resultado').value = "0"
+    }
+}
